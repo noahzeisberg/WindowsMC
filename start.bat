7f1364484175b719b6cbb5b87c6a4f857bd44324
NoahOnFyre <noahonfyre@gmail.com>
2023-05-08 14:53:11 +0200

    Added checker, setup and install server sections.

--- start.bat
+++ start.bat
@@ -1,3 +1,23 @@
 :INIT
 @echo off
 title WindowsMC - Minecraft's start.bat reimagined
+
+:CHECKER
+if not exist winmc_config.txt goto SETUP
+set server_dir=<winmc_config.txt
+cd %server_dir%
+
+:SETUP
+set /p server_dir_path="Enter path to server folder: "
+echo server_dir_path > winmc_config.txt
+set server_dir=<winmc_config.txt
+cd %server_dir%
+if not exist server.jar goto INSTALL_SERVER
+mkdir WindowsMC
+
+:INSTALL_SERVER
+cd %server_dir%
+set /p selected_version="Enter minecraft version: "
+curl "https://papermc.io/api/v1/paper/%selected_version%/latest/download" --output server.jar
+
+:MAIN
